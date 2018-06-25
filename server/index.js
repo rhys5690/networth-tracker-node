172b19d7b1e88370bc62f1d9d0c0b87d7edb7b7e
rhys5690 <rhys5690@gmail.com>
2018-06-26 07:40:11 +1000

    added field to graphql api

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
   type Query {
     hello(name: String): String!
+    bankTotal: [BankTotal!]!
+  }
+
+  type BankTotal {
+    networthTotal: String!
   }
 `
 
@@ -10,6 +15,9 @@ const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
   },
+  BankTotal: {
+    networthTotal: () => 'blahhhh'
+  }
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers })
